fd730ae09740fd7bab2e5c5cf2db07c2ba6a56a0
Varma15102005 <ldvkvarma@gmail.com>
2025-06-18 18:08:57 +0530

    Delete contact.js

--- contact.js
+++
@@ -1,8 +0,0 @@
-function sendMail(){
-    let parms={
-        name:document.getElementById('nameinput1').value,
-        email:document.getElementById('emailInput').value,
-        message:document.getElementById('messageInput').value,
-    }
-    emailjs.send("service_2zs6kg2","template_lv02zbr",parms).then(alert("email sent"));
-}
